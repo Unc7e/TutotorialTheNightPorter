4dc405a8d2953be35a8717232382c64eff9b3d1a
Leon <yanglyu.leon.7@gmail.com>
2018-08-02 14:37:30 -0700

    demo: Adding or Configuring the StackView

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -87,7 +87,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.title = "First ViewController"
+        self.title = "TableView Demo"
     }
 
     override func didReceiveMemoryWarning() {
